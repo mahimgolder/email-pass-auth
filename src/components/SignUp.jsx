ae9267004de54fb5c8e683bb99c624ca3901ead7
mahimgolder <mahimgolder@mahim>
2024-12-14 23:23:12 +0600

    pass regEx & validation

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,21 +3,43 @@ import React, { useState } from 'react';
 import { auth } from '../firebase/firebase.init';
 
 const SignUp = () => {
+  // error massage var
     const [errorMess, setErrorMess] = useState('');
     
+    // when user is successfully create account ,display show an error
+    const [success, setSuccess] = useState(false);
+    
+    // form submit func
     const handleSubmit = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const pass = e.target.password.value;
-        setErrorMess('')
+        setErrorMess('');
+        setSuccess('');
+
+        // pass validation section here
+        if(pass.length < 6){
+          setErrorMess('Please set at least 6 carectar');
+          return;
+        }
+        const passwordPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{5,}$/;
+        if(!passwordPattern.test(pass)){
+          setErrorMess("please enter: one number, one uppercase, one lowercase, one spacial carectar for your security");
+          return;
+        }
+
+        // create user email and pass section here
         createUserWithEmailAndPassword(auth, email, pass )
         .then(res => {
-            console.log(res.user)
+          console.log(res.user);
+          setSuccess(true);
         }).catch(error => {
             console.log(errorMess);
             setErrorMess(error.message);
         })
     }
+
+
     return (
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
           <form onSubmit={handleSubmit} className="card-body">
@@ -36,9 +58,15 @@ const SignUp = () => {
                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
               </label>
             </div>
+            
+            {/* error show in display */}
             {
                 errorMess && <p>{errorMess}</p>
             }
+            {
+              success && <p>Successfully Created account</p>
+            }
+           
             <div className="form-control mt-6">
               <button className="btn btn-primary">Login</button>
             </div>
